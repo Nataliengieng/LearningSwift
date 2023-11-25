b5c2c50fcd95b16d8f831b148649eec55e823678
Natalie Ng <ngcheokieng@yahoo.com.hk>
2023-11-25 16:55:02 +0000

    completed "Handling errors"

---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by nat on 25/11/2023.
+//
+
+import Foundation
---
+++ Scrumdinger/Views/ErrorView.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by nat on 25/11/2023.
+//
+
+import Foundation
