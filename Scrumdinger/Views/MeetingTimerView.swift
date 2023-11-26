6cc736a19ff1555cb2435ddc2cf87e5d8c617815
Natalie Ng <ngcheokieng@yahoo.com.hk>
2023-11-26 11:11:45 +0000

    completed "Drawing the timer view" and updated README.md

---
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -0,0 +1,8 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by nat on 26/11/2023.
+//
+
+import Foundation
---
+++ Scrumdinger/Views/SpeakerArc.swift
@@ -0,0 +1,8 @@
+//
+//  SpeakerArc.swift
+//  Scrumdinger
+//
+//  Created by nat on 26/11/2023.
+//
+
+import Foundation
